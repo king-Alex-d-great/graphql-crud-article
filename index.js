d6d578670a479150feaadc8884371a7ef16a862c
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-04-14 15:22:09 +0100

    feat: add express server

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { connectDB } = require("./database");
+const { RootQuery } = require("./queries");
+const { GraphQLSchema } = require("graphql");
+const { seedData } = require("./seeddata");
+
+const app = express();
+
+//Conect to mongoDB
+connectDB();
+seedData();
+
+const schema = new GraphQLSchema({
+    query: RootQuery
+})
+
+//pass the schema to our GraphQL middleware:
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        graphiql: true,
+        schema
+    })
+);
+
+app.get("/", (_, res) =>
+    res.status(200).send({
+        message: "Welcome, graphQL genius!",
+    })
+);
+
+app.listen(7010, () => {
+    console.log("Listening on 7010");
+});
